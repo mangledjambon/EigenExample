f86dd0babf87f122b07f451faffd73a86d64f007
mangledjambon <MotorHead666>
2015-05-07 16:39:17 +0100

    began working on vector/matrix operations for Sound Separation

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main()
 {
+    /*
     MatrixXd m1(2,2);
     MatrixXd m2(2,2);
 
@@ -19,12 +20,10 @@ int main()
     m2(0,1) = 3;
     m2(1,1) = m1(1,0) + m2(0,1);
 
-
-    std::cout << "m1" << std::endl << m1 << std::endl;
-    std::cout << "m2" << std::endl << m2 << std::endl;
+    cout << "m1" << std::endl << m1 << std::endl;
+    cout << "m2" << std::endl << m2 << std::endl;
 
     MatrixXd m3 = MatrixXd::Random(3,3);
-
     m3 = m3 + MatrixXd::Constant(3,3,1.2) * 50;
 
     cout << "m3 = " << endl << m3 << endl;
@@ -33,8 +32,33 @@ int main()
 
     v << 1, 2, 3; //put values into vector
 
-    cout << "m * v = " << endl << m3 * v << endl;
+    cout << "m3 * v = " << endl << m3 * v << endl;
+    */
+
+
+    //============= New Stuff =================//
+
+    int numIters = 100;
+    int np = 3;
+    Vector3f pan_dir(0, (const float)M_PI/4.f, (const float)M_PI/2.f);
+
+    /* // details
+    cout    << "Pan:" << endl
+            << pan_dir << endl
+            << "SIN:" << endl << pan_dir.array().sin() << endl
+            << "COS:" << endl << pan_dir.array().cos();
+
+    */
+
+    MatrixXf projmat = MatrixXf::Zero(3,2);
+    MatrixXf panmat = MatrixXf::Zero(3,2);
+    projmat << pan_dir.array().sin(), -(pan_dir.array().cos());
+    panmat << pan_dir.array().cos(), pan_dir.array().sin();
+
+    MatrixXf k = MatrixXf::Zero(3,3);
+    k = panmat.transpose() * projmat;
+    cout << k;
 
     return 0;
 
-}
+};
